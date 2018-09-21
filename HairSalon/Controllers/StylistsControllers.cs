e8efca37ae8aeeed7b6b70ec2aed98f13b008e84
Catherine Bradley <catherinetybradley@gmail.com>
2018-09-21 09:44:23 -0700

    add links to a details client page for stylists

--- HairSalon/Controllers/StylistsControllers.cs
+++ HairSalon/Controllers/StylistsControllers.cs
@@ -30,5 +30,16 @@ namespace HairSalon.Controllers
       //Redirects to Index() after posting. This prevents the bug of adding the same items when refreshing after posting. Index doesn't refer to the index.cshtml but rather the function called Index() in this controller. You can add a second parameter after this to specify which controller if needed.
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult Details(int id)
+    {
+      // Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      // List<MagicCard> categoryCards = selectedStylist.GetMagicCards();
+      // model.Add("category", selectedStylist);
+      // model.Add("magiccards", categoryCards);
+      return View("Details");
+    }
   }
 }
---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    private int _id;
+    private string _name;
+
+    public Client(string newName, int newId =0)
+    {
+      _id = newId;
+      _name = newName;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO clients (name) VALUES (@ClientName);";
+
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@ClientName";
+      name.Value = _name;
+      cmd.Parameters.Add(name);
+
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+
+        Client newClient = new Client(clientName, clientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allClients;
+    }
+
+
+  }
+}
